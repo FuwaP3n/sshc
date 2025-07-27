00f352131f9a9f88b9cbe8e23b2bc351ac15b4f3
Fuwa Pen <fuwa.pen.official@gmail.com>
2025-07-27 19:10:06 +0300

    Tested string.h at this stage fixed issues only with string.h. We should recheck and test main client.c and fix issues

--- src/string.c
+++ src/string.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
+#include <stdio.h>
 int strlen(char *a){
 	int length = 0;
 	while(a[length]!='\0'){
 		length++;
 	}
-	return length; //!NB! It will return index of last element excluding \0 char!
+	return length; //!NB! It will return length of string without \0 character, so string "Hello world!\0" will return int 12
 }
 
 char * strcomb(char *a, char *b){
@@ -23,16 +24,34 @@ char * strcomb(char *a, char *b){
 		o++;
 	}
 	out[size_out] = '\0';
-	return out; //!!!MEMORY LEAK IF YOU NOT FREE THIS POINTER!!
+	return out;
 }
 
-int strcmp(char *str1, char *str2){
+bool strcmp(char *str1, char *str2){
 	if(strlen(str1)==strlen(str2)){
-		for(int i=0; i<strlen(str1)+1; i++){
+		for(int i=0; i<strlen(str1); i++){
 			if(str1[i]!=str2[i]){
-				return 0;
+				return false;
 			}
-		}	return 1;
+		}	return true;
 	}	
-	return 0;
+	return false;
 }
+
+
+
+/*TEST MAIN
+int main(){
+	printf("Measuring length of a string...\nstring: 'Hello world!'\n");
+	printf("%d\n", strlen("Hello world!\0"));
+	printf("Combining strings...\nstring1: 'Hello '  |  string2: 'world!'\n");
+	printf("%s\n", strcomb("Hello \0", "world!\0"));
+	printf("Comparing two strings...\nstring1: 'Hello' |  string2: 'world!'\nPredicted false\n");
+	if(strcmp("Hello\0", "world!\0")){ printf("true\n"); }
+	else { printf("false\n"); }
+	printf("Comparing two strings...\nstring1: 'Hello world!' |  string2: 'Hello world!'\nPredicted true\n");
+	if(strcmp("Hello world!\0", "Hello world!\0")){ printf("true\n"); }
+	else { printf("false\n"); }
+	printf("TEST FINISHED!\n");
+
+}*/
